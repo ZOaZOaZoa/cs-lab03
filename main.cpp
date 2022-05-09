1b3fd95ad772fd85bd4ac76f630c1e4a9d6439c6
ZOaZOaZoa <vbifbk555@gmail.com>
2022-05-09 15:15:49 +0300

    code: added option to download data from internet site

--- main.cpp
+++ main.cpp
@@ -44,26 +44,55 @@ Input read_input(istream& in, bool prompt)
     return data;
 }
 
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
+{
+    size_t data_size = item_size * item_count;
+
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<char*>(items), data_size);
+
+    return data_size;
+}
+
+
+Input download(const string& address)
+{
+    stringstream buffer;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+      CURLcode res;
+      curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+      res = curl_easy_perform(curl);
+      if (res != CURLE_OK)
+      {
+          cerr << "Error: " << curl_easy_strerror(res);
+          exit(1);
+      }
+      curl_easy_cleanup(curl);
+    }
+
+    return read_input(buffer, false);
+}
+
 
 int main(int argc, char* argv[])
 {
+    Input input;
     if(argc > 1)
     {
-        curl_global_init(CURL_GLOBAL_ALL);
-        CURL *curl = curl_easy_init();
-        if(curl) {
-          CURLcode res;
-          curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-          res = curl_easy_perform(curl);
-          curl_easy_cleanup(curl);
-        }
-        return 0;
+        input = download(argv[1]);
+    }
+    else
+    {
+        input = read_input(cin, true);
     }
 
-
-
-    const auto data = read_input(cin, 1);
-    const auto bins = make_histogramm(data);
+    const auto bins = make_histogramm(input);
     show_histogramm_svg(bins);
 
     return 0;
